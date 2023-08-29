a83630aaa94ff5655bccb560bc479ded2e0bff61
SandyAstorga <sandyastorga13@gmail.com>
2023-08-29 13:59:49 -0600

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import MainPage from './components/MainPage/MainPage'
 
+
 function App() {
   return (
     <div className='App'>
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,51 +1,101 @@
-import { useRef } from 'react';
+/* eslint-disable no-unused-vars */
+import { useRef, useState } from 'react';
 // import validation from '../Contact/validation'
+import { toast } from 'react-toastify';
 import emailjs from '@emailjs/browser';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
   const form = useRef();
+  const [formData, setFormData ] = useState({
+  user_name: '',
+  user_email: '',
+  message: '',
+})
 
   const sendEmail = (e) => {
     e.preventDefault();
-
+    
+    // Envía el correo
     emailjs.sendForm('service_jocd60n', 'template_7gnvw1s', form.current, 'OlF1TmjkTsP_ofzms')
       .then((result) => {
         console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
+  
+        // Después de enviar el correo, muestra una notificación
+        toast.success('Mensaje Enviado con Exito', {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 3000,
+        });
+  
+        // Limpia el formulario
+        setFormData({
+          user_name: '',
+          user_email: '',
+          message: '',
+        });
+      })
+      .catch((error) => {
+        console.error(error.text);
+        
+        // Si ocurre un error, puedes mostrar una notificación de error si lo deseas
+        toast.error('Error al enviar el mensaje', {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 3000,
+        });
       });
   };
-
+  
   return (
-    <div className=" bg-purple-black h-screen ml-auto py-6 bg-gradient-to-r from-wine">
-      <div className="animate-fade-in-y ml-4 mr-52 px-5 mt-5">
-        <h1 className="text-white font-rubikG text-2xl animate-fade-in-x">¡Gracias por visitar mi perfil y espero podamos trabajar juntos pronto!</h1>
+      <div className="bg-purple-black md:h-screen py-6 bg-gradient-to-r from-wine">
+        <div className="md:flex md:items-center md:space-x-9 md:px-11 mt-5 ml-9 mr-9">
+          <div className="md:w-1/2">
+            <h1 className="text-white font-rubikG text-xl animate-fade-in-y">
+              ¡Gracias por visitar mi perfil y espero que podamos trabajar juntos pronto!
+            </h1>
+          </div>
+          {/* <div className="md:w-1/2"> */}
+            <img
+              src="./src/assets/mensaje.png"
+              alt=""
+              className="animate-fade-in-y md:mr-6 w-24 h-24 hover:scale-110"
+            />
+          {/* </div> */}
+        </div>
+        <form
+          ref={form}
+          onSubmit={sendEmail}
+          className="ml-9 md:mt-9 font-handjet text-white font-bold text-xl py-4 mr-9 animate-fade-in-x"
+        >
+          <label className="block mt-4">Nombre:</label>
+          <input
+            className="md:w-1/2 rounded-lg text-center text-black"
+            placeholder="Ingresar Nombre"
+            type="text"
+            name="user_name"
+          />
+          <label className="block mt-4 ">Email:</label>
+          <input
+            className="md:w-1/2 rounded-lg text-center text-black"
+            placeholder="Ingresar Email"
+            type="email"
+            name="user_email"
+          />
+          <label className="block mt-4 ">Mensaje:</label>
+          <textarea
+            className="md:w-1/2 rounded-lg mt-2 text-center text-black"
+            placeholder="Mensaje"
+            name="message"
+            rows="4"
+            cols="32"
+          />
+          <button
+            value="Send"
+            type='submit'
+            className="bg-purple text-white hover:text-purple-black font-bold font-handjet text-xl h-10 w-24 rounded-lg hover:scale-110 ml-10"
+          > Enviar </button>
+        </form>
       </div>
-      <img src="./src/assets/mensaje.png" alt="" className="animate-fade-in-x ml-auto w-36 h-36 mr-11 -mt-16 hover:scale-110" />
-      <form ref={form} onSubmit={sendEmail} className=" animate-fade-in-y font-handjet text-white font-bold text-2xl py-4 mr-11 ml-5">
-        <label className="mr-6 ml-6">Nombre:</label>
-        <input className="mr-52 rounded-lg text-center ml-2 text-black"
-          placeholder="Ingresar Nombre"
-          type="text"
-          name="user_name"
-        />
-        <label className="block mr-6 ml-6 w-16 mt-6">Email:</label>
-        <input className=" absolute -mt-6 ml-28 rounded-lg text-center text-black"
-          placeholder="Ingresar Email"
-          type="email"
-          name="user_email"
-        />
-        <label className="block mr-6 ml-6 w-20 mt-6">Mensaje:</label>
-        <textarea className="mt-2 rounded-lg ml-6 text-center text-black"
-          placeholder="Mensaje"
-          name="message" 
-          rows='5'
-          cols='50'
-        />
-        <button  value="Send" className=" bg-purple text-white hover:text-purple-black font-bold font-handjet text-2xl h-11 w-44 rounded-lg hover:scale-110 ml-6">Enviar</button>
-      </form>
-    </div>
-  )
+    );
 }
 
 export default Contact;
